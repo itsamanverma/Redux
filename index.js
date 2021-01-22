2f0b705722e11b5a8fe0c2c51d018f145f1c80f1
itsamanverma <amanvermame786@gmail.com>
2021-01-22 13:18:53 +0530

    perforemed the multiple action using single reducers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake() {
     return {
@@ -10,10 +11,19 @@ function buyCake() {
     }
 }
 
+function buyIcecream() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Second Redux Action',
+    }
+}
+
+
 //Reducers
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20,
 }
 
 const reducer = (state = initialState, action) => {
@@ -21,6 +31,10 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
         }
         default: return state
     }
@@ -32,4 +46,7 @@ const unsubscribe = store.subscribe(() => console.log('updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
 unsubscribe()
